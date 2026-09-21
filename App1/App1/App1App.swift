d30481fd3478ae8db38d3b398a481d4939495847
Petr Homola <homola.p@gmail.com>
2026-09-21 08:58:42 +0100

    Workshop Sep 2026 (#1)
    * app1
    
    * structured output
    
    * tool calling example

---
+++ App1/App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Petr Homola on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1/App1/Views/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Petr Homola on 01/09/2026.
+//
+
+import SwiftUI
+
+enum FocusedField {
+    case prompt
+}
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("hello_world")
+            }
+            .padding()
+                .tabItem {
+                    Label("hello_world", systemImage: "globe")
+                }
+            SimplePromptView()
+                .tabItem {
+                    Label("simple_prompt", systemImage: "1.square")
+                }
+            StructuredOutputView()
+                .tabItem {
+                    Label("structured_output", systemImage: "2.square")
+                }
+            ToolCallingView()
+                .tabItem {
+                    Label("tool_calling", systemImage: "3.square")
+                }
+        }
+        .tabViewStyle(.sidebarAdaptable)
+    }
+}
+
+#Preview {
+    ContentView()
+}
